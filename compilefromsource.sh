6f86d18132af77f1ac65b37bd03a63874a1d0a50
xuanlilei <xuanlilei@t2cloud.net>
2023-02-13 22:15:38 +0800

    fix compile

--- compilefromsource.sh
+++ compilefromsource.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #  compilefromsource.sh
-#  V2RayX
+#  V2RayXS
 #
 #  Created by Cenmrev on 10/15/16.
 #  Copyright © 2016 Cenmrev. All rights reserved.
@@ -32,24 +32,23 @@ BOLD='\033[1m'
 NORMAL='\033[0m'
 datetime=$(date "+%Y-%m-%dTIME%H%M%S")
 if [[ ! -f /Applications/Xcode.app/Contents/MacOS/Xcode ]]; then
-    echo "${RED}Xcode is needed to build V2RayX, Please install Xcode from App Store!${NORMAL}"
-    echo "${RED}编译 V2RayX 需要 Xcode.app，请从 App Store 里安装 Xcode.${NORMAL}"
+    echo "${RED}Xcode is needed to build V2RayXS, Please install Xcode from App Store!${NORMAL}"
+    echo "${RED}编译 V2RayXS 需要 Xcode.app，请从 App Store 里安装 Xcode.${NORMAL}"
 else
     echo "${BOLD}-- Downloading source code --${NORMAL}"
     echo "${BOLD}-- 正在下载源码 --${NORMAL}"
-    git clone --recursive https://github.com/Cenmrev/V2RayX.git "V2RayX${datetime}"
-    cd "V2RayX${datetime}"
-    echo "${BOLD}-- Start building V2RayX --${NORMAL}"
-    echo "${BOLD}-- 开始编译 V2RayX --${NORMAL}"
-    xcodebuild -project V2RayX.xcodeproj -target V2RayX -configuration Release
+    git clone --recursive https://github.com/lileixuan/V2RayXS.git "V2RayXS${datetime}"
+    cd "V2RayXS${datetime}"
+    echo "${BOLD}-- Start building V2RayXS --${NORMAL}"
+    echo "${BOLD}-- 开始编译 V2RayXS --${NORMAL}"
+    xcodebuild -project V2RayXS.xcodeproj -target V2RayXS -configuration Release
     if [[ $? == 0 ]]; then
         echo "${GREEN}-- Build succeeded --${NORMAL}"
         echo "${GREEN}-- 编译成功 --${NORMAL}"
-        echo "${BOLD}V2RayX.app: $(pwd)/build/Release/V2RayX.app${NORMAL}"
+        echo "${BOLD}V2RayXS.app: $(pwd)/build/Release/V2RayXS.app${NORMAL}"
     else
         echo "${RED}-- Build failed --${NORMAL}"
         echo "${RED}-- 编译失败 --${NORMAL}"
     fi
 fi
-
 
